cb3d9d3dcd855d36fe3395685983876af4f60550
Julien Castillo <juliencastillo77@gmail.com>
2024-10-14 11:37:43 +0200

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,7 +49,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4e48431e2b8d1f99105aa83f745efa88
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2d9e125fa1c3b3a3edc236870611d4df",
+"flutter_bootstrap.js": "28814c49b7c76a55cc41916ffc3699b4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
