67ca6961abdcca4b26227be1227c3da2af012bad
Julien Castillo <juliencastillo77@gmail.com>
2024-10-14 23:49:12 +0200

    build with photos of musicians (strings only)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "274432882"
+    serviceWorkerVersion: "2718071409"
   }
 });
