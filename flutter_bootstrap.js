731f6a4e0ce939754f547e90670e5b86e97d074d
Julien Castillo <juliencastillo77@gmail.com>
2024-11-09 23:06:50 +0100

    add admin features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2795774586"
+    serviceWorkerVersion: "14042134"
   }
 });
